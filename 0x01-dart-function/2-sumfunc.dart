0d77a377f7cd26981a37855603d0fbee1480df19
CaramonH <caramonh@yahoo.com>
2024-06-10 12:07:32 -0500

    add, sub, and showFunc

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -2,3 +2,16 @@
 //add() that accepts two arguments a, b and return the sum
 //sub() that accepts two arguments a, b and return Subtraction of them
 //showFunc() that returns a message with the output of the two other function
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+String showFunc(int a, int b) {
+    int sum = add(a, b);
+    int difference = sub(a, b);
+    return 'Add $a + $b = $sum\nSub $a -$b = $difference';
+}
