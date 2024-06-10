8be810b93f6da8c4d4028cf1c2c262b3ff38b972
CaramonH <caramonh@yahoo.com>
2024-06-10 12:44:19 -0500

    simplified function

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,8 +1,8 @@
 //Given two maps that represents the number of successful shots for each shot type in a basketball game between team A and Team B.
 //function whoWins() that calculates the total number of points for each team and return the winning team
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int teamAPoints = (teamA['Free Throw'] ?? 0) + (teamA['2 Pointer'] ?? 0) * 2 + (teamA['3 Pointer'] ?? 0) * 3;
-  int teamBPoints = (teamB['Free Throw'] ?? 0) + (teamB['2 Pointer'] ?? 0) * 2 + (teamB['3 Pointer'] ?? 0) * 3;
+  var teamAPoints = teamA['Free throws']! * 1 + teamA['2 pointers']! * 2 + teamA['3 pointers']! * 3;
+  var teamBPoints = teamB['Free throws']! * 1 + teamB['2 pointers']! * 2 + teamB['3 pointers']! * 3;
 
   if (teamAPoints > teamBPoints) {
     return 1;
