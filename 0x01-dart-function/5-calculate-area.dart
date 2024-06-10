ce3419eda255b0d3b545b456c7b02da3481bf40f
CaramonH <caramonh@yahoo.com>
2024-06-10 12:25:21 -0500

    calculate area

--- 0x01-dart-function/5-calculate-area.dart
+++ 0x01-dart-function/5-calculate-area.dart
@@ -1,1 +1,5 @@
-//function calculateArea() that accepts two variables height and base and returns the area of the triangle.
+//function calculateArea() that accepts two variables height and base and returns the area of the triangle
+double calculateArea(double height, double base) {
+  double area = 0.5 * height * base;
+  return double.parse(area.toStringAsFixed(2)); // rounding to two decimal places
+}
