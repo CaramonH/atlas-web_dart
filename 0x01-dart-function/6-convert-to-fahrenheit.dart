6eaea8cd71837ea3b3f4e46ebbcce036d10eb9d0
CaramonH <caramonh@yahoo.com>
2024-06-10 12:31:22 -0500

    celcius to fahrenheit

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,2 +1,5 @@
 //function convertToF() that accepts a list of doubles representing a list of temperatures in celsius,
 // converts them to fahrenheit and returns them in a list.
+List<double> convertToF(List<double> temperaturesInC) {
+  return temperaturesInC.map((temp) => double.parse(((temp * 9 / 5) + 32).toStringAsFixed(2))).toList();
+}
