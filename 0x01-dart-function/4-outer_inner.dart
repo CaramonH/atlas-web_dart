58073bca6577ab680e90aa9ac236b53e22ca225e
CaramonH <caramonh@yahoo.com>
2024-06-10 12:21:02 -0500

    fixed naming convention

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -5,9 +5,11 @@ void outer(String name, String id) {
   String inner() {
     List<String> parts = name.split(' ');
     String lastNameInitial = parts[0][0];
-    String firstName = parts[1];
-    return 'Hello Agent $lastNameInitial.$firstName your id is $id';
+    String firstNameInitial = parts[1][0];
+    String lastName = parts[1];
+    return 'Hello Agent $lastNameInitial.$firstNameInitial$lastName your id is $id';
   }
   
   print(inner());
 }
+
