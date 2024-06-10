25238244f0f14e6f041d82804b42ec09608b78a7
CaramonH <caramonh@yahoo.com>
2024-06-10 12:51:45 -0500

    first attempt

--- 0x01-dart-function/9-palindrome.dart
+++ 0x01-dart-function/9-palindrome.dart
@@ -1,1 +1,14 @@
 //function isPalindrome() that accepts one argument String and returns True if the input String is a palindrome otherwise it returns False
+bool isPalindrome(String s) {
+  if (s.length < 3) {
+    return false;
+  }
+  
+  for (int i = 0; i < s.length ~/ 2; i++) {
+    if (s[i] != s[s.length - 1 - i]) {
+      return false;
+    }
+  }
+  
+  return true;
+}
