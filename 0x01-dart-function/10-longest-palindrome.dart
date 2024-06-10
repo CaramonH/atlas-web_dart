1920d307d9bc05eb656aa62def6ac86e910fbe68
CaramonH <caramonh@yahoo.com>
2024-06-10 12:55:00 -0500

    Implement function to find the longest palindrome substring

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,1 +1,27 @@
 //function longestPalindrome() that accepts one argument String and returns the longest palindrome substring.
+String longestPalindrome(String s) {
+  if (s.length < 3) {
+    return 'none';
+  }
+
+  String longest = '';
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 2; j <= s.length; j++) {
+      String substring = s.substring(i, j);
+      if (isPalindrome(substring) && substring.length > longest.length) {
+        longest = substring;
+      }
+    }
+  }
+
+  return longest.isNotEmpty ? longest : 'none';
+}
+
+bool isPalindrome(String s) {
+  for (int i = 0; i < s.length ~/ 2; i++) {
+    if (s[i] != s[s.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
