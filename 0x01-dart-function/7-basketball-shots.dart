6d80bd9aff884baeb5247b90b0ea93ce8e37b9c3
CaramonH <caramonh@yahoo.com>
2024-06-10 12:42:35 -0500

    Add function to calculate the winning basketball team

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,2 +1,14 @@
 //Given two maps that represents the number of successful shots for each shot type in a basketball game between team A and Team B.
 //function whoWins() that calculates the total number of points for each team and return the winning team
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  int teamAPoints = (teamA['Free Throw'] ?? 0) + (teamA['2 Pointer'] ?? 0) * 2 + (teamA['3 Pointer'] ?? 0) * 3;
+  int teamBPoints = (teamB['Free Throw'] ?? 0) + (teamB['2 Pointer'] ?? 0) * 2 + (teamB['3 Pointer'] ?? 0) * 3;
+
+  if (teamAPoints > teamBPoints) {
+    return 1;
+  } else if (teamAPoints < teamBPoints) {
+    return 2;
+  } else {
+    return 0;
+  }
+}
