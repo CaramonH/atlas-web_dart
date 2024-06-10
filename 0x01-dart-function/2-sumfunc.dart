cfbf4ff5610b80afa54dfa5fb19a415de03e912c
CaramonH <caramonh@yahoo.com>
2024-06-10 12:10:20 -0500

    fixing subtract output

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -3,15 +3,17 @@
 //sub() that accepts two arguments a, b and return Subtraction of them
 //showFunc() that returns a message with the output of the two other function
 int add(int a, int b) {
-    return a + b;
+  int res = a + b;
+  return res;
 }
 
 int sub(int a, int b) {
-    return a - b;
+  int res = a - b;
+  return res;
 }
 
 String showFunc(int a, int b) {
-    int sum = add(a, b);
-    int difference = sub(a, b);
-    return 'Add $a + $b = $sum\nSub $a -$b = $difference';
+  int sum = add(a, b);
+  int difference = sub(a, b);
+  return 'Add $a + $b = $sum\nSub $a - $b = $difference';
 }
