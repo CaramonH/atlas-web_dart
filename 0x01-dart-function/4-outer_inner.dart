515c9c97681ce3bee24e57a47481f1478a455de1
CaramonH <caramonh@yahoo.com>
2024-06-10 12:19:27 -0500

    two funcs, take arg and print arg

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,3 +1,13 @@
 //two functions:
 //outer() take two argument name, id
 //print the output of the inside function inner()
+void outer(String name, String id) {
+  String inner() {
+    List<String> parts = name.split(' ');
+    String lastNameInitial = parts[0][0];
+    String firstName = parts[1];
+    return 'Hello Agent $lastNameInitial.$firstName your id is $id';
+  }
+  
+  print(inner());
+}
