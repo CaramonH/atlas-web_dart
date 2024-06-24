f16354109bf1a50d2821949e2f20f7c1ffb9818f
CaramonH <caramonh@yahoo.com>
2024-06-24 15:34:39 -0500

    changing password to access password the right way

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,27 +1,27 @@
 import '6-password.dart';
 
 class User extends Password {
-  int? id;
-  String? name;
-  int? age;
-  double? height;
+  String name;
+  int age;
+  double height;
+  int id;
 
-  User(
-      {required this.id,
-      required this.name,
-      required this.age,
-      required this.height,
-      required String user_password})
-      : super(password: user_password);
+  User({
+    required this.name,
+    required this.age,
+    required this.height,
+    required this.id,
+    String user_password = '',
+  }) : super(password: user_password);
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      id: json['id'] ?? 0,
-      name: json['name'] ?? '',
-      age: json['age'] ?? 0,
-      height: json['height'] ?? 0.0,
-      user_password: json['user_password'] ?? '',
-    );
+  String get user_password => this.password;
+
+  set user_password(String newPassword) {
+    this.password = newPassword;
+  }
+
+  String showName() {
+    return 'Hello $name';
   }
 
   Map<String, dynamic> toJson() {
@@ -33,12 +33,18 @@ class User extends Password {
     };
   }
 
-  @override
-  String toString() {
-    return 'User(id: $id, name: $name, age: $age, height: $height, ${super.toString()})';
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'] as int? ?? 0,
+      name: userJson['name'] as String? ?? 'Unknown',
+      age: userJson['age'] as int? ?? 0,
+      height: userJson['height'] as double? ?? 0.0,
+      user_password: userJson['user_password'] as String? ?? '',
+    );
   }
 
-  set user_password(String value) {
-    _password = value;
+  @override
+  String toString() {
+    return 'User(id : $id ,name: $name, age: $age, height: $height, Password: ${isValid()})';
   }
 }
